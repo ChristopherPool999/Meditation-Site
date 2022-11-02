f48f002f687f039f1cef395d0d6fb72e42824a51
ChristopherPool999 <chrispool228@gmail.com>
2022-11-01 23:26:32 -0700

    started working on responsiveness for mobile

--- meditation.js
+++ meditation.js
@@ -1,29 +1,23 @@
 "use strict";
 import { stopwatch } from "./modules/stopwatch.js";
 
-const clockInterface = document.querySelectorAll(".timers");
-let playIconDetermination = document.querySelector(".play__button__icon").classList;
-let mainClock = new stopwatch();
-let btnPresses = 0;
-let timeOfThoughts = document.querySelector(".thought__times");
+const clockInterface = document.querySelectorAll(".time");
+const playIconDetermination = document.querySelector(".play__button__icon").classList;
+const timeOfThoughts = document.querySelector(".analysis__info");
+const lostFocusText = document.querySelector("#lost__focus__count");
+const mainClock = new stopwatch();
 let lostFocusAmount = 0; 
-let lostFocusText = document.querySelector("#number__of__thoughts");
+let btnPresses = 0;
 
-mainClock.onTimerEnd = () => {
-    let audio = new Audio("./images_sound/alarm.mp3");
-    audio.play();
-    playIconDetermination.toggle("active"); 
-}
-
-document.addEventListener("keydown", function(input) {
-    if (!isNaN(parseInt(input.key)) && !mainClock.hasStarted) 
-        if ((input.key !== "0" || !mainClock.isEmpty)) {
-            mainClock.isEmpty = false;
-            mainClock.setInterface(input.key, clockInterface);
-            mainClock.seconds = mainClock.convertToSeconds(mainClock.interface);
-            mainClock.updateInterface();
-        }
-    if (input.key === "Backspace" && !mainClock.isEmpty) {
+const dynamicEventListener = action => {
+    if (!isNaN(parseInt(action)) && !mainClock.hasStarted) 
+    if ((action !== "0" || !mainClock.isEmpty)) {
+        mainClock.isEmpty = false;
+        mainClock.setInterface(action, clockInterface);
+        mainClock.seconds = mainClock.convertToSeconds(mainClock.interface);
+        mainClock.updateInterface();
+    }
+    if (action === "Backspace" && !mainClock.isEmpty) {
         btnPresses++;
         setTimeout(function() {
             btnPresses = 0;
@@ -35,15 +29,15 @@ document.addEventListener("keydown", function(input) {
             mainClock.reset();
             mainClock.updateInterface(clockInterface);
         }
-    }
-    if (input.key === "Enter" && !mainClock.isActive && !mainClock.isEmpty) {
+        }
+    if (action === "Enter" && !mainClock.isActive && !mainClock.isEmpty) {
         mainClock.hasStarted = true;
         mainClock.isActive = true;
         mainClock.seconds = mainClock.convertToSeconds(mainClock.interface);
         mainClock.countDown(clockInterface);
         playIconDetermination.toggle("active"); 
     }
-    else if (input.key === "Enter" && mainClock.isActive) {
+    else if (action === "Enter" && mainClock.isActive) {
         let recordedTime = mainClock.interface[0] + mainClock.interface[1] + ":" + mainClock.interface[2] + 
                 mainClock.interface[3] + ":" + mainClock.interface[4] + mainClock.interface[5];
         timeOfThoughts.innerHTML += `<span>
@@ -51,12 +45,33 @@ document.addEventListener("keydown", function(input) {
                         </span>`;
         lostFocusText.innerHTML = "Lost focus: " + ++lostFocusAmount;
     }
-    if (input.code === "Space" && mainClock.hasStarted) {
+    if (action === "Space" && mainClock.hasStarted) {
         mainClock.isActive ? mainClock.isActive = false : mainClock.countDown(clockInterface);
         playIconDetermination.toggle("active");
     }
+}
+
+document.addEventListener("keydown", function(input) {
+    input.code === "Backspace" || input.code === "Space" ? dynamicEventListener(input.code) 
+        : dynamicEventListener(input.key);
 })
 
-// fix scalable div container
+document.addEventListener("click", function(event) {
+    if (event.target.classList[0] === "play__button__icon") {
+        !mainClock.isActive ? dynamicEventListener("Enter") : dynamicEventListener("Space");
+    }
+})
 
-// change
+if (document.addEventListener("click", function() {
+    console.log("foo");
+}))
+
+mainClock.onTimerEnd = () => {
+    let audio = new Audio("./images_sound/alarm.mp3");
+    audio.play();
+    playIconDetermination.toggle("active"); 
+}
+
+// make responsive
+// make it better looking --- look into UX design and other timer designs 
+// functionality for FAQ, login, about
